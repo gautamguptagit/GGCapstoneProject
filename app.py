2417cd0f86e2b3b8c47b2a9dca83312f0c2fc671
EC2 Default User <ec2-user@ip-172-31-28-192.ec2.internal>
2022-07-18 22:00:13 +0000

    pylint

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
-print("Hello GG, Welcome to Python..")
-print("This is Udacity Program")
+# This program prints Hello, world!
+print("This program is for final project submission.")
+print("Gautam Gupta, Hello All")
