b6bacada118d946d415303735ea43c2c16d5df02
EC2 Default User <ec2-user@ip-172-31-28-192.ec2.internal>
2022-07-19 09:14:36 +0000

    Final Push to create a new cluster

--- aws_kube_infra.sh
+++ aws_kube_infra.sh
@@ -1,8 +1,8 @@
-occurance=$(grep -i eksctl-gg-capstone output.txt | wc -l)
+occurance=$(grep -i eksctl-gg-capston output.txt | wc -l)
 if [[ $occurance -gt 0 ]]
     then 
     echo "Since Infra is already existing hence not creating Clstr"
     else
     echo "creat AWS kube clustr"
-    eksctl create cluster --name gg-capstone-udacity --version 1.21 --region $AWS_DEFAULT_REGION --nodegroup-name capstone --node-type t3.large --nodes-min 2 --nodes-max 3 --managed
+    eksctl create cluster --name gg-capston-udacity --version 1.21 --region $AWS_DEFAULT_REGION --nodegroup-name capstone --node-type t3.large --nodes-min 2 --nodes-max 3 --managed
 fi
